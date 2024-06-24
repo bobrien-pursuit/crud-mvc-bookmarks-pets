1299a8fa6b4314dd82dc30d346eaa75d1ab4a88c
Brendan O'Brien <bobrien@pursuit.org>
2024-06-24 12:38:22 -0400

    creates post

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -5,7 +5,7 @@ const petsArray = require('../models/pet')
 
 // localhost:4001/pets/
 pets.get('/', (req, res) => {
-    res.json(petsArray)
+    res.json(petsArray);
 })
 
 //localhost:4401/pets
@@ -17,6 +17,12 @@ pets.get(`/:arrayIndex`, (req, res) => {
     res.status(404).json({error: "Not Found"});
 
 });
+
+//sends a new pet
+pets.post(`/`, (req, res) => {
+    petsArray.push(req.body);
+    res.status(201).json(petsArray[petsArray.length-1]);
+})
 
 
 
